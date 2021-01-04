f9b5824f9e834cdaa1d193c2a244aeb48a7b9faf
huchx <296101967@qq.com>
2021-01-04 17:12:19 +0800

    JDBCTemplate配置文件连接数据库

--- spring-boot-jdbctemplate/src/main/java/com/huchx/jdbctemplate/JdbcTemplateApp.java
+++ spring-boot-jdbctemplate/src/main/java/com/huchx/jdbctemplate/JdbcTemplateApp.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * Hello world!
  *
  */
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})//动态连接数据库时使用
+@SpringBootApplication
 public class JdbcTemplateApp
 {
     public static void main( String[] args )
--- spring-boot-jdbctemplate/src/main/java/com/huchx/jdbctemplate/service/UserService.java
+++ spring-boot-jdbctemplate/src/main/java/com/huchx/jdbctemplate/service/UserService.java
@@ -1,5 +1,6 @@
 package com.huchx.jdbctemplate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Service;
@@ -10,22 +11,68 @@ import java.util.Map;
 @Service
 public class UserService {
 
-   private JdbcTemplate initJdbcTemplate(){
-       DriverManagerDataSource dataSource = new DriverManagerDataSource();
-       dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-       dataSource.setUrl("jdbc:mysql://localhost:3306/spring_boot?serverTimezone=UTC");
-       dataSource.setUsername("root");
-       dataSource.setPassword("123456");
-       JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-       return jdbcTemplate;
-   }
+    // =====================================================================
+    // **************************动态连接数据库使用**************************
+    // =====================================================================
+//   private JdbcTemplate initJdbcTemplate(){
+//       DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//       dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//       dataSource.setUrl("jdbc:mysql://localhost:3306/spring_boot?serverTimezone=UTC");
+//       dataSource.setUsername("root");
+//       dataSource.setPassword("123456");
+//       JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+//       return jdbcTemplate;
+//   }
+//
+//    /**
+//     * 查询
+//     * @return
+//     */
+//    public List<Map<String,Object>> select() {
+//        JdbcTemplate jdbcTemplate = initJdbcTemplate();
+//        String sql = "Select * from m_user";
+//        List<Map<String,Object>> users = jdbcTemplate.queryForList(sql);
+//        return users;
+//    }
+//
+//    /**
+//     * 插入
+//     */
+//    public void insert(String name) {
+//        JdbcTemplate jdbcTemplate = initJdbcTemplate();
+//        String sql = "insert into m_user (id,name) values (null,?)";
+//        jdbcTemplate.update(sql,name);
+//    }
+//
+//    /**
+//     * 更新
+//     */
+//    public void update(int id,String name) {
+//        JdbcTemplate jdbcTemplate = initJdbcTemplate();
+//        String sql = "update m_user set name = ? where id = ?";
+//        jdbcTemplate.update(sql,name,id);
+//    }
+//
+//    /**
+//     * 删除
+//     */
+//    public void delete(int id) {
+//        JdbcTemplate jdbcTemplate = initJdbcTemplate();
+//        String sql = "delete from m_user where id = ?";
+//        jdbcTemplate.update(sql,id);
+//    }
 
-    /**
+    // ============================================================================
+    // **************************配置文件连接数据库使用部分**************************
+    // ============================================================================
+   @Autowired
+   JdbcTemplate jdbcTemplate;
+        /**
      * 查询
      * @return
      */
     public List<Map<String,Object>> select() {
-        JdbcTemplate jdbcTemplate = initJdbcTemplate();
+
         String sql = "Select * from m_user";
         List<Map<String,Object>> users = jdbcTemplate.queryForList(sql);
         return users;
@@ -35,7 +82,6 @@ public class UserService {
      * 插入
      */
     public void insert(String name) {
-        JdbcTemplate jdbcTemplate = initJdbcTemplate();
         String sql = "insert into m_user (id,name) values (null,?)";
         jdbcTemplate.update(sql,name);
     }
@@ -44,7 +90,6 @@ public class UserService {
      * 更新
      */
     public void update(int id,String name) {
-        JdbcTemplate jdbcTemplate = initJdbcTemplate();
         String sql = "update m_user set name = ? where id = ?";
         jdbcTemplate.update(sql,name,id);
     }
@@ -53,7 +98,6 @@ public class UserService {
      * 删除
      */
     public void delete(int id) {
-        JdbcTemplate jdbcTemplate = initJdbcTemplate();
         String sql = "delete from m_user where id = ?";
         jdbcTemplate.update(sql,id);
     }
